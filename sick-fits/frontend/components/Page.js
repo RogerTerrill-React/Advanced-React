0a9ff4c23e01fd6888ece3c1714d9f82f5d0a152
Roger Terrill <rchicasterill@gmail.com>
2018-12-19 11:00:04 -0800

    Global Styeling and Typography with Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 const theme = {
   red: '#FF0000',
@@ -15,13 +15,43 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: ${props=> props.theme.black};
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    /* border-box means that height and width includes padding and margin */
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    /* Since font-size is 10, 1.5rem is now 15px */
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
